83a426c0c3766a5b185f1a0e200358699ed3a0bb
shrish <adhikari.shrish1@gmail.com>
2025-12-02 15:47:00 +0545

    Update AWS CLI installation script: replace outdated package update command with installation of curl and unzip, and add system upgrade step for improved setup reliability.

--- AWS/scripts/ubuntu/install-awscli.sh
+++ AWS/scripts/ubuntu/install-awscli.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo apt-get update curl
+sudo apt-get install -y curl unzip
+sudo apt-get -y upgrade
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
 sudo ./aws/install
